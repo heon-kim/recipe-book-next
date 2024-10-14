37fdca47cb6df56ca87155c5733da6d939fcafc1
heon-kim <vndtka123@gmail.com>
2024-10-14 15:11:59 +0900

    💄 login CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import './globals.css';
 
 export default function RootLayout({
   children,
@@ -6,19 +6,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      
+    <html lang='en'>
       <body>
-      <header className="flex justify-between">
-        <h1>나만의 레시피</h1>
-        <div><button>레시피 추가</button><button>로그아웃</button></div>
-      </header>
+        <header className='flex justify-between'>
+          <h1>나만의 레시피</h1>
+          <div>
+            <button>레시피 추가</button>
+            <button>로그아웃</button>
+          </div>
+        </header>
         {children}
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-       
-       </footer>
+        <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'></footer>
       </body>
-      
     </html>
   );
 }
--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -1,50 +1,78 @@
+import Image from 'next/image';
+import { signIn } from '@/auth';
 
-import { signIn } from "@/auth"
-import Image from "next/image"
- 
 export default function SignIn() {
   return (
-    <div className="shadow-md p-8 flex flex-col gap-4">
-    <h2 className="text-lg font-bold">로그인</h2>
-    <form action="">
-        <label htmlFor="email">
-            <span>이메일</span>
-            <p>
-        <input type="email" name="email" id="email" placeholder="your-email@example.com"/>
-        </p>
+    <div className='w-96 shadow-md p-8 flex flex-col gap-6 rounded-md'>
+      <h2 className='text-lg font-bold'>로그인</h2>
+      <form action='' className='flex flex-col gap-3'>
+        <label htmlFor='email'>
+          <p className='mb-1'>이메일</p>
+          <input
+            className='border rounded-md p-2 w-full'
+            type='email'
+            name='email'
+            id='email'
+            placeholder='your-email@example.com'
+          />
         </label>
-        
-        <label htmlFor="password">
-            <p>비밀번호</p>  
-        <input type="password" name="password" id="password" placeholder="비밀번호를 입력하세요"/>
-        </label>
-        
 
-        <button className="block bg-blue-500 text-white">로그인</button>
-    </form>
-    <form
-      action={async () => {
-        "use server"
-        await signIn("github")
-      }}
-    >
-      <button type="submit" className="flex items-center gap-2 border rounded-md p-3">
-        <Image src='/img/github-mark.svg' alt="github mark" width={16} height={16}></Image>
-        <span>Sign In with GitHub</span>
+        <label htmlFor='password'>
+          <p className='mb-1'>비밀번호</p>
+          <input
+            className='border rounded-md p-2 w-full'
+            type='password'
+            name='password'
+            id='password'
+            placeholder='비밀번호를 입력하세요'
+          />
+        </label>
+
+        <button className='block p-2 bg-blue-500 text-white rounded-md'>
+          로그인
         </button>
-    </form>
-    <form
-      action={async () => {
-        "use server"
-        await signIn("google")
-      }}
-    >
-      <button type="submit" className="flex items-center gap-2 border rounded-md p-3">
-        <Image src='/img/google-mark.png' alt="google mark" width={16} height={16}></Image>
-        <span>Sign In with Google</span>
-        </button>
-    </form>
+      </form>
+
+      <div className='flex flex-col gap-2'>
+        <form
+          action={async () => {
+            'use server';
+            await signIn('github');
+          }}
+        >
+          <button
+            type='submit'
+            className='w-full flex items-center justify-center gap-2 border rounded-md p-3'
+          >
+            <Image
+              src='/img/github-mark.svg'
+              alt='github mark'
+              width={16}
+              height={16}
+            ></Image>
+            <span>Sign In with GitHub</span>
+          </button>
+        </form>
+        <form
+          action={async () => {
+            'use server';
+            await signIn('google');
+          }}
+        >
+          <button
+            type='submit'
+            className='w-full flex items-center justify-center gap-2 border rounded-md p-3'
+          >
+            <Image
+              src='/img/google-mark.png'
+              alt='google mark'
+              width={16}
+              height={16}
+            ></Image>
+            <span>Sign In with Google</span>
+          </button>
+        </form>
+      </div>
     </div>
-    
-  )
-} 
+  );
+}
